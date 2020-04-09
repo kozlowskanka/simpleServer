d9f547d48ffd8f6a56e1d3864c331aa5c67f9d50
Anka Kozłowska <kozlowskanka@gmail.com>
2020-04-09 18:07:19 +0200

    Code correction

--- server.js
+++ server.js
@@ -4,30 +4,34 @@ const path = require('path');
 const app = express();
 
 app.use((req, res, next) => {
-    res.show = (name) => {
-      res.sendFile(path.join(__dirname + `/views/${name}`));
-    };
-    next();
-  });
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname + `/views/${name}`));
+  };
+  next();
+});
 
-  app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.static(path.join(__dirname + '/public')));
+
+app.use('/user', (req, res) => {
+  res.show('forbidden.html');
+});
   
-  app.get('/', (req, res) => {
-    res.show('home.html');
-  });
+app.get('/', (req, res) => {
+  res.show('home.html');
+});
+
+app.get('/home', (req, res) => {
+  res.show('home.html');
+});
   
-  app.get('/about', (req, res) => {
-    res.show('about.html');
-  });
-  
-  app.use('/user', (req, res) => {
-    res.show('forbidden.html');
-  });
-  
-  app.use((req, res) => {
-    res.status(404).show('404.html');
-  });
-  
-  app.listen(8000, () => {
-    console.log('Server is running on port: 8000');
-  });
+app.get('/about', (req, res) => {
+  res.show('about.html');
+});
+
+app.use((req, res) => {
+  res.status(404).show('404.html');
+});
+
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
